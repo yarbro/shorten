490b7a6a233aff4bf067f96ecfdf3189d56bb34d
David Yarbro <y4rbr0@gmail.com>
2018-08-16 15:29:25 -0500

    Error handling for Twilio api

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,10 +4,14 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
 
     if @message.valid?
-      TextMessenger.new(@message.number, redirect_link_url(@link.name)).deliver
-      redirect_to links_path, notice: 'SMS sent successfully!'
+      if TextMessenger.new(@message.number, redirect_link_url(@link.name)).deliver
+        redirect_to links_path, notice: 'SMS sent successfully!'
+      else
+        flash.now[:error] = 'SMS could not be sent please try again.'
+        render 'links/show'
+      end
     else
-      flash.now[:error] = 'SMS could not be sent please try again.'
+      flash.now[:error] = 'There was an error on the form please retry.'
       render 'links/show'
     end
   end
--- app/services/text_messenger.rb
+++ app/services/text_messenger.rb
@@ -5,12 +5,17 @@ class TextMessenger
   end
 
   def deliver
-    client = Twilio::REST::Client.new
-    client.messages.create({
-      from: '+15128293862',
-      to: @number,
-      body: "Hi, check out this awesome website: #{@url}"
-    })
+    begin
+      client = Twilio::REST::Client.new
+      client.messages.create({
+        from: '+15128293862',
+        to: @number,
+        body: "Hi, check out this awesome website: #{@url}"
+      })
+      return true
+    rescue Twilio::REST::RestError
+      return false
+    end
   end
 
   private

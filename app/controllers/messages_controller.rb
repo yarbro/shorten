a4778aa9d86074e43ba457d96c31f683abd7b2ff
David Yarbro <y4rbr0@gmail.com>
2018-08-16 14:46:57 -0500

    Add Twilio production credentials

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
 
     if @message.valid?
-      TextMessenger.new(@message.number, redirect_link_url(@link.name))
+      TextMessenger.new(@message.number, redirect_link_url(@link.name)).deliver
       redirect_to links_path, notice: 'SMS sent successfully!'
     else
       flash.now[:error] = 'SMS could not be sent please try again.'
--- app/services/text_messenger.rb
+++ app/services/text_messenger.rb
@@ -7,7 +7,7 @@ class TextMessenger
   def deliver
     client = Twilio::REST::Client.new
     client.messages.create({
-      from: '+15005550006',
+      from: '+15128293862',
       to: @number,
       body: "Hi, check out this awesome website: #{@url}"
     })
